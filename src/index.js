75364dd049717040258a3e86a620f83cdb190875
ManuelEdesa <war4zone@gmail.com>
2022-02-06 18:37:23 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//este archivo arranca el codigo de los otros archivos
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
