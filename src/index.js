3da2a58e9038ca8808ba2f630a7e21a365ced1bd
ManuelEdesa <war4zone@gmail.com>
2022-02-06 18:37:50 +0100

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //este archivo arranca el codigo de los otros archivos
 import React from "react";
 import ReactDOM from "react-dom";
-
+//llama a  la funcion app del archivo App
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
