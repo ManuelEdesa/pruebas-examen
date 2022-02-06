23bb61669e5ca1c16ffd41f34b4a9c99122ee0de
ManuelEdesa <war4zone@gmail.com>
2022-02-06 19:08:45 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import React from "react";
+//aqui se importa el archivo css que le da las propiedades a la funcion App
 import "./style.css";
 
 export default function App() {
@@ -7,6 +8,17 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <h2>hola </h2>
     </div>
   );
 }
+
+
+return (
+  <div className="App2">
+    <h2>HOLA PRUEBA 2</h2>
+    </div>
+);
+
+
+export default App2;
